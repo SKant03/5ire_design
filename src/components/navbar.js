9e3efec36b4bfc5d584956a02174788d7c26c0b0
SKant03 <shashikant040203@gmail.com>
2023-03-27 19:20:40 +0530

    added missing file

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+export default function Navbar() {
+  return (
+    <div className="Navmenu">
+        <div className="logo">
+
+        </div>
+        <div className="menu">
+
+        </div>
+        
+    </div>
+
+  )
+}
