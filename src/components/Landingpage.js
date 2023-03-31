3887892c6e75bead74a13571a095b7401cdc0397
prajwal <prajwalpandey26@gmail>
2023-04-01 01:03:02 +0530

    igiveup

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -23,16 +23,16 @@ const Landingpage = () => {
 
 
     <div className="">
-          <nav className=" my-8 flex justify-center items-center">
+          <nav className=" my-8 flex gap-20 justify-center items-center">
             <div className="flex justify-start">
-              <div className=" flex w-2/5 justify-start">
+              <div className=" flex w-3/5 justify-start">
                 <img src={Vector_1} alt="" className="md:w-3/5" />
                 <p className="flex items-center text-transparent text-6xl bg-clip-text bg-gradient-to-r from-indigo-400 to-white ">
                   5ire
                 </p>
               </div>
             </div>
-            <div className="text-2xl text-white flex items-center justify-between w-5/6">
+            <div className="text-2xl text-white flex items-center gap-20">
               <div className="flex">
                 <Dropdown title="About Us" className="">
                   <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-10 absolute">
@@ -120,28 +120,29 @@ const Landingpage = () => {
           </nav>
         </div>
 
-        <div className=" -60 text-center  my-10 flex flex-col gap-10 w-full justify-center items-center z-0">
-          <p className="text-transparent text-8xl bg-clip-text bg-gradient-to-r from-[#77FFE7] to-white z-0">
+        <div className="text-center  my-20 flex flex-col gap-5 items-center z-0">
+          <p className="text-transparent text-4xl md:text-8xl bg-clip-text bg-gradient-to-r from-[#77FFE7] to-white z-0">
             BLOCKCHAIN
           </p>
-          <p className="text-transparent text-8xl bg-clip-text bg-gradient-to-r from-[#77FFE7] to-indigo-400">
+          <p className="text-transparent text-4xl md:text-8xl bg-clip-text bg-gradient-to-r from-[#77FFE7] to-indigo-400">
             MEETS
           </p>
-          <p className="text-transparent text-8xl bg-clip-text bg-gradient-to-r from-indigo-400 to-white ">
+          <p className="text-transparent text-4xl md:text-8xl bg-clip-text bg-gradient-to-r from-indigo-400 to-white ">
             SUSTAINABLITY
           </p>
         </div>
 
         {/* cards */}
-        <section className=" -3/4 flex justify-between w-4/5 mx-40 my-10">
+        <section className="flex justify-between mx-20 md:my-20">
           <div>
           <div className="container">
-            <div className="box">
-              <span className="title">GLASS EFFECT</span>
-              <div>
-                <strong>JOE WATSON SBF</strong>
-                <p>0000 000 000 0000</p>
-                <span>VALID</span> <span>01/28</span>
+            <div className="box text-white flex items-center">
+            <img src={Vector_1} alt="" className="md:w-1/5 rounded-full" />
+              <NavLink to="/testnet" className="title">Testnet Beta</NavLink>
+              <div className="flex flex-col">
+                <strong></strong>
+                <p></p>
+                <span></span> <span></span>
               </div>
             </div>
           </div>
@@ -149,25 +150,27 @@ const Landingpage = () => {
 
           <div>
           <div className="container">
-            <div className="box">
-              <span className="title">GLASS EFFECT</span>
-              <div>
-                <strong>JOE WATSON SBF</strong>
-                <p>0000 000 000 0000</p>
-                <span>VALID</span> <span>01/28</span>
+            <div className="box text-white flex items-center">
+            <img src={Vector_1} alt="" className="md:w-1/5 rounded-full" />
+              <NavLink to='/documentation' className="title">5ire Documentation</NavLink>
+              <div className="flex flex-col">
+                <strong></strong>
+                <p></p>
+                <span></span> <span></span>
               </div>
             </div>
           </div>
           </div>
-
           <div>
+
           <div className="container">
-            <div className="box">
-              <span className="title">GLASS EFFECT</span>
-              <div>
-                <strong>JOE WATSON SBF</strong>
-                <p>0000 000 000 0000</p>
-                <span>VALID</span> <span>01/28</span>
+            <div className="box text-white flex items-center">
+            <img src={Vector_1} alt="" className="md:w-1/5 rounded-full" />
+              <NavLink to="/paertnerwithus" className="title">Partner with us</NavLink>
+              <div className="flex flex-col">
+                <strong></strong>
+                <p></p>
+                <span></span> <span></span>
               </div>
             </div>
           </div>
