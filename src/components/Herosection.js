606b93ad2cc2617a08c117a330d59a8f88542500
prajwal <prajwalpandey26@gmail>
2023-03-31 22:29:15 +0530

    Herosection

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -16,6 +16,7 @@ const Herosection = () => {
           loop="{true}"
           className="md:w-screen md:h-full h-screen object-cover"
         />
+
         <div>
           <nav className="absolute top-0 my-8 flex w-full justify-center items-center">
             <div className="flex justify-start">
@@ -29,56 +30,84 @@ const Herosection = () => {
             <div className="text-3xl text-white flex items-center justify-between w-5/6 top-0 mx-6">
               <div className="flex">
                 <Dropdown title="About Us" className="absolute">
-                <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-p">
-                    <DropdownItem className="py-4 mx-4">About 5ire</DropdownItem>
+                  <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-p">
+                    <DropdownItem className="py-4 mx-4">
+                      About 5ire
+                    </DropdownItem>
                     <DropdownItem className="py-4 mx-4">Team</DropdownItem>
                     <DropdownItem className="py-4 mx-4">Blog</DropdownItem>
-                    <DropdownItem className="py-4 mx-4">Road to Manifest</DropdownItem>
-                    <DropdownItem className="py-4 mx-4">News & Media</DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      Road to Manifest
+                    </DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      News & Media
+                    </DropdownItem>
                   </div>
                 </Dropdown>
               </div>
               <div className="flex">
                 <Dropdown title="Testnet" className="absolute bg-transparent ">
-                  <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl
-                  w-max my-4 absolute z-50">
+                  <div
+                    className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl
+                  w-max my-4 absolute z-50"
+                  >
                     <DropdownItem className="py-4 mx-4">Testnet</DropdownItem>
-                    <DropdownItem className="py-4 mx-4">Build To Earn</DropdownItem>
-                    <DropdownItem className="py-4 mx-4">Documentaion</DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      Build To Earn
+                    </DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      Documentaion
+                    </DropdownItem>
                   </div>
                 </Dropdown>
               </div>
               <div>
                 <Dropdown title="Community" className="absolute bg-transparent">
                   <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-4 absolute z-50">
-                  <DropdownItem className="py-4 mx-4">Community</DropdownItem>
-                  <DropdownItem className="py-4 mx-4">5ire Ambassdor</DropdownItem>
-                  <DropdownItem className="py-4 mx-4">Partner with Us</DropdownItem>
+                    <DropdownItem className="py-4 mx-4">Community</DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      5ire Ambassdor
+                    </DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      Partner with Us
+                    </DropdownItem>
                   </div>
                 </Dropdown>
               </div>
               <div>
                 <Dropdown title="Research" className="absolute bg-transparent">
                   <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-4 absolute z-50">
-                  <DropdownItem className="py-4 mx-4">Reasearch Papers</DropdownItem>
-                  <DropdownItem className="py-4 mx-4">Yellow paper</DropdownItem>
-                  <DropdownItem className="py-4 mx-4">White paper</DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      Reasearch Papers
+                    </DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      Yellow paper
+                    </DropdownItem>
+                    <DropdownItem className="py-4 mx-4">
+                      White paper
+                    </DropdownItem>
                   </div>
                 </Dropdown>
               </div>
               <div>
                 <Dropdown title="Validator" className="absolute bg-transparent">
-                <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-4">
-                  <DropdownItem className="py-4 mx-4">Become a Validator</DropdownItem>
+                  <div className="bg-gradient-to-l from-[#457387] to-[#10171B] p-4 rounded-xl text-xl w-max my-4">
+                    <DropdownItem className="py-4 mx-4">
+                      Become a Validator
+                    </DropdownItem>
                   </div>
                 </Dropdown>
               </div>
               <div>
-                <NavLink to="/sustainablity" className="absolute">Sustainablity</NavLink>
-                
+                <NavLink to="/sustainablity" className="absolute">
+                  Sustainablity
+                </NavLink>
               </div>
               <div className="relative top-4">
-                <NavLink to="/5ire Explorer" className="bg-[#01FFA4] px-8 mx-6 text-xl h-[50px] font-semibold flex justify-end items-center rounded-full text-black">
+                <NavLink
+                  to="/5ire Explorer"
+                  className="bg-[#01FFA4] px-8 mx-6 text-xl h-[50px] font-semibold flex justify-end items-center rounded-full text-black"
+                >
                   5ire Explorer
                 </NavLink>
               </div>
@@ -97,6 +126,50 @@ const Herosection = () => {
             SUSTAINABLITY
           </p>
         </div>
+
+        {/* cards */}
+        <section className="absolute top-3/4 flex justify-between w-4/5 mx-40 my-10">
+          <div>
+          <div className="container">
+            <div className="box">
+              <span className="title">GLASS EFFECT</span>
+              <div>
+                <strong>JOE WATSON SBF</strong>
+                <p>0000 000 000 0000</p>
+                <span>VALID</span> <span>01/28</span>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div>
+          <div className="container">
+            <div className="box">
+              <span className="title">GLASS EFFECT</span>
+              <div>
+                <strong>JOE WATSON SBF</strong>
+                <p>0000 000 000 0000</p>
+                <span>VALID</span> <span>01/28</span>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div>
+          <div className="container">
+            <div className="box">
+              <span className="title">GLASS EFFECT</span>
+              <div>
+                <strong>JOE WATSON SBF</strong>
+                <p>0000 000 000 0000</p>
+                <span>VALID</span> <span>01/28</span>
+              </div>
+            </div>
+          </div>
+          </div>
+
+
+        </section>
       </div>
     </>
   );
