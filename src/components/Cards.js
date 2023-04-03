ea43ee60dd16781a3dc9242c99c5b835650031ef
prajwal <prajwalpandey26@gmail>
2023-04-03 23:56:23 +0530

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,8 +15,9 @@ const Cards = () => {
       {/* cards */}
       <section className="flex justify-between mx-32 2xl:mx-60 mt-24 ">
             <div>
-              <div className="container">
-                <div className="box text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-white flex items-center">
+              <div className="container my-10 relative before:bg-[#5E51F3] before:absolute before:rounded-[50%] before:w-[6rem]
+              before:h-[6rem] before:top-[30%] before:right-[7%] after:bg-[#5E51F3] after:absolute after:h-[3rem] after:top-[8%] after:right-[5%] after:border ">
+                <div className="box py-[40px] bg-[#000000] backdrop:blur-3xl rounded-[20px] transition-all flex flex-col items-center hover:border relative">
                   <img
                     src={Vector}
                     alt=""
@@ -24,45 +25,6 @@ const Cards = () => {
                   />
                   <NavLink to="/testnet" className="title">
                     Testnet Beta
-                  </NavLink>
-                  <div className="flex flex-col">
-                    <strong></strong>
-                    <p></p>
-                    <span></span> <span></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <div className="container">
-                <div className="box text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-white flex items-center">
-                  <img
-                    src={Vector}
-                    alt=""
-                    className="w-1/5 rounded-full"
-                  />
-                  <NavLink to="/documentation" className="title">
-                    5ire Documentation
-                  </NavLink>
-                  <div className="flex flex-col">
-                    <strong></strong>
-                    <p></p>
-                    <span></span> <span></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="container">
-                <div className="box text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-white flex items-center">
-                  <img
-                    src={Vector}
-                    alt=""
-                    className="w-1/5 rounded-full"
-                  />
-                  <NavLink to="/paertnerwithus" className="title">
-                    Partner with us
                   </NavLink>
                   <div className="flex flex-col">
                     <strong></strong>
