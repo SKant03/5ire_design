8322a4904335c3599a7a3ad7038719f506101dc9
prajwal <prajwalpandey26@gmail>
2023-04-04 03:05:36 +0530

    hero section partial update

--- src/components/Buildtoearn.js
+++ src/components/Buildtoearn.js
@@ -3,33 +3,33 @@ import build1 from "../assets/build1.png";
 
 const Buildtoearn = () => {
   return (
-<div className="flex justify-center items-center my-28">
-<div class="card px-12">
-      <div className="text-white flex flex-col gap-20">
-        <p className="text-center text-6xl font-semibold">#BuildToEarn</p>
-        <div className="flex">
-          <div className="flex flex-col gap-10 text-2xl">
-            <li>Top 10 dApps to receive an incentive after evaluation.</li>
-            <li>
-              An opportunity to earn rewards through our referral program and
-              grants scheme.
-            </li>
-            <li>
-              An incentivization scheme for dApps focused on sustainable
-              development.
-            </li>
-            <li>
-              Reduced gas fees for early adopters and the first 500 builders to
-              receive a share of block rewards.
-            </li>
-          </div>
-          <div>
-            <img src={build1} alt="" className="w-11/12" />
+    <div className="flex justify-center items-center my-28">
+      <div class="card px-12">
+        <div className="text-white flex flex-col gap-20">
+          <p className="text-center text-8xl font-semibold">#BuildToEarn</p>
+          <div className="flex">
+            <div className="flex flex-col gap-10 text-3xl">
+              <li>Top 10 dApps to receive an incentive after evaluation.</li>
+              <li>
+                An opportunity to earn rewards through our referral program and
+                grants scheme.
+              </li>
+              <li>
+                An incentivization scheme for dApps focused on sustainable
+                development.
+              </li>
+              <li>
+                Reduced gas fees for early adopters and the first 500 builders
+                to receive a share of block rewards.
+              </li>
+            </div>
+            <div>
+              <img src={build1} alt="" className="w-11/12" />
+            </div>
           </div>
         </div>
       </div>
     </div>
-</div>
   );
 };
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,36 +7,126 @@ import videoBg from "../assets/videoBg.mp4";
 import { NavLink } from "react-router-dom";
 import Header from "./Header";
 import Email from "./Email";
-import Vector from "../assets/Vector.png";
+import image2 from "../assets/image2.png";
+import image1 from "../assets/image1.png";
 
 const Cards = () => {
   return (
     <div>
       {/* cards */}
-      <section className="flex justify-between mx-32 2xl:mx-60 mt-24 ">
-            <div>
-              <div className="container my-10 relative before:bg-[#5E51F3] before:absolute before:rounded-[50%] before:w-[6rem]
-              before:h-[6rem] before:top-[30%] before:right-[7%] after:bg-[#5E51F3] after:absolute after:h-[3rem] after:top-[8%] after:right-[5%] after:border ">
-                <div className="box py-[40px] bg-[#000000] backdrop:blur-3xl rounded-[20px] transition-all flex flex-col items-center hover:border relative">
-                  <img
-                    src={Vector}
+      <section className="mx-32 2xl:mx-60 mt-24 ">
+        <div>
+        <div className="flex justify-evenly">
+          <div>
+            <div
+              className="container my-10 relative before:bg-[#5E51F3] before:absolute before:rounded-[50%] before:w-[6rem]
+              before:h-[6rem] before:top-8 before:right-3 after:bg-[#5E51F3] after:absolute after:h-[3rem] after:top-4 after:right-3 after:border "
+            >
+              <div className="box h-[720px] w-[720px] py-[40px] bg-[#5E51F3] backdrop:blur-3xl rounded-[20px] transition-all flex flex-col items-center hover:border relative">
+                <div className="my-10 flex flex-col items-center">
+                  <div className="title text-center text-white flex flex-col items-center gap-4">
+                    <h className="text-8xl">Government</h>
+                    <br />
+                    <img
+                    src={image1}
                     alt=""
-                    className="w-1/5 rounded-full"
+                    className="rounded-full my-4 w-2/5"
                   />
-                  <NavLink to="/testnet" className="title">
-                    Testnet Beta
-                  </NavLink>
-                  <div className="flex flex-col">
-                    <strong></strong>
-                    <p></p>
-                    <span></span> <span></span>
+                    <p>
+                      5ire provides a novel way for governments to engage with
+                      citizens and scale their capacities for effective
+                      administration with transparency, efficiency, and speed.
+                    </p>
                   </div>
+                  
                 </div>
               </div>
             </div>
-          </section>
-    </div>
-  )
-}
+          </div>
+          <div>
+          <div
+              className="container my-10 relative before:bg-[#5E51F3] before:absolute before:rounded-[50%] before:w-[6rem]
+              before:h-[6rem] before:top-8 before:right-3 after:bg-[#5E51F3] after:absolute after:h-[3rem] after:top-4 after:right-3 after:border "
+            >
+              <div className="box h-[720px] w-[720px] py-[40px] bg-[#5E51F3] backdrop:blur-3xl rounded-[20px] transition-all flex flex-col items-center hover:border relative">
+                <div className="my-10 flex flex-col items-center">
+                  <div className="title text-center text-white flex flex-col items-center gap-2">
+                    <h className="text-8xl">Fortune 500</h>
+                    <br />
+                    <img
+                    src={image2}
+                    alt=""
+                    className="rounded-full my-4 w-2/6"
+                  />
+                    <p>
+                    Fortune 500 companies have a disproportionate impact on the future of the world's sustainability. By encoding SGDs and ESG in our blockchain, 5ire makes it easy for businesses to do good.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
 
-export default Cards
+        <div>
+        <div className="flex justify-evenly">
+          <div>
+            <div
+              className="container my-10 relative before:bg-[#5E51F3] before:absolute before:rounded-[50%] before:w-[6rem]
+              before:h-[6rem] before:top-8 before:right-3 after:bg-[#5E51F3] after:absolute after:h-[3rem] after:top-4 after:right-3 after:border "
+            >
+              <div className="box h-[720px] w-[720px] py-[40px] bg-[#5E51F3] backdrop:blur-3xl rounded-[20px] transition-all flex flex-col items-center hover:border relative">
+                <div className="my-10 flex flex-col items-center">
+                  <div className="title text-center text-white flex flex-col items-center gap-4">
+                    <h className="text-8xl">Universities</h>
+                    <br />
+                    <img
+                    src={image1}
+                    alt=""
+                    className="rounded-full my-4 w-2/5"
+                  />
+                    <p>
+                    5ire helps universities leverage the power of sustainability and make their administration and operations digitalized, real-time and transparent.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+          <div
+              className="container my-10 relative before:bg-[#5E51F3] before:absolute before:rounded-[50%] before:w-[6rem]
+              before:h-[6rem] before:top-8 before:right-3 after:bg-[#5E51F3] after:absolute after:h-[3rem] after:top-4 after:right-3 after:border "
+            >
+              <div className="box h-[720px] w-[720px] py-[40px] bg-[#5E51F3] backdrop:blur-3xl rounded-[20px] transition-all flex flex-col items-center hover:border relative">
+                <div className="my-10 flex flex-col items-center">
+                  <div className="title text-center text-white flex flex-col items-center gap-2">
+                    <h className="text-7xl">Social Entrepreneurs</h>
+                    <br />
+                    <img
+                    src={image2}
+                    alt=""
+                    className="rounded-full my-4 w-2/6"
+                  />
+                    <p>
+                    5ire is fast, secure, and cheap. This makes us an ideal ecosystem for entrepreneurs who want to combine impact with business as they scale their mission.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+
+      </section>
+    </div>
+  );
+};
+
+export default Cards;
